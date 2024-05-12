e4c88b3d128b4aaafcdba59646575cdbf5d8b74a
Gabby-Tech1 <gkaquainoo@gmail.com>
2024-05-12 17:08:41 +0000

    Done with the navbar and hero section of the recipe site

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+import Recipe from './Components/Recipe'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Recipe/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Recipe.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Recipe = () => {
+  return (
+    <div>
+        <div className='text-center'>
+            <h1 className='text-3xl font-bold tracking-wider '>Popular <span className='text-orange-500'>Recipes</span> You Can't Miss</h1>
+            <p>This is the recipe page</p>
+        </div>
+    </div>
+  )
+}
+
+export default Recipe
