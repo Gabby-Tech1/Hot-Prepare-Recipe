eca4291409a168a97dc46a44290dff900f17ccce
Gabby-Tech1 <gkaquainoo@gmail.com>
2024-05-17 22:38:52 +0000

    Solved some bugs

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import hero from '../assets/hero.png'
+import { HashLink as Link } from 'react-router-hash-link'
 
 const Hero = () => {
   return (
@@ -9,14 +10,14 @@ const Hero = () => {
                 <h1 className='md:text-5xl text-3xl font-bold'>
                     Cook Like A Pro with Our <span className='text-orange-500'>Easy</span> and <span className='text-orange-500'>Tasty</span> Recipes
                 </h1>
-                <p className='text-gray-600 italic font-medium'>Dive into our treasury of delectable recipes crafted with passion and care. From comforting classics to innovative delights, each dish promises a symphony of taste that will tantalize your senses and warm your heart. Join us as we embark on a culinary adventure where every recipe tells a story and every bite is a celebration of life's delicious moments. Let's create magic in the kitchen together and savor the joy of cooking!
-
+                <p className='text-gray-600 italic font-medium'>
+                    Dive into our treasury of delectable recipes crafted with passion and care. Join us as we embark on a culinary adventure where every recipe tells a story and every bite is a celebration of life's delicious moments. Let's create magic in the kitchen together and savor the joy of cooking!
                 </p>
-                <a href="" className='bg-orange-500 w-fit px-10 py-2 lg:mt-4 rounded-3xl text-white font-medium tracking-wide hover:scale-110 duration-500 ease-linear'>Our Offers</a>
+                <Link to="#recipe" smooth className='bg-orange-500 w-fit px-10 py-2 lg:mt-4 rounded-3xl text-white font-medium tracking-wide hover:scale-110 duration-500 ease-linear'>Our Offers</Link>
             </div>
 
             <div>
-                <img src={hero} alt="" className='lg:w-[970px] '/>
+                <img src={hero} alt="" className='lg:w-[1070px] '/>
             </div>
         </div>
     </div>
